7dfc0df029e3c4b7ca0448c7b7f783397ff75a65
David S. Ferreira <74317650+davidsf026@users.noreply.github.com>
2022-09-13 13:21:40 -0300

    Update oc-nexus-provision.sh

--- oc-provisioning/oc-nexus-provision.sh
+++ oc-provisioning/oc-nexus-provision.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 function deploy() {
-  oc new-project oqss-cicd --display-name="CI/CD"
-  
-  sleep 2
-  
-  oc new-app -f ./application/template.yaml -n oqss-cicd  
+    echo "############################################################"
+    echo "Importing image"
+    echo "############################################################"
+    
+    oc new-project oqss-cicd --display-name="CI/CD"
+    sleep 2
+    
+    oc new-app -f ./application/template.yaml -n oqss-cicd
 }
