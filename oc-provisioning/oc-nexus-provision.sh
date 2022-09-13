26ecb5d180ad7978e7aec7c6a10d30c49abdddff
David S. Ferreira <74317650+davidsf026@users.noreply.github.com>
2022-09-13 13:48:24 -0300

    Update oc-nexus-provision.sh

--- oc-provisioning/oc-nexus-provision.sh
+++ oc-provisioning/oc-nexus-provision.sh
@@ -6,12 +6,12 @@ echo "#  $ oc login http://console.your.openshift.com                           
 echo "###############################################################################"
 
 function deploy() {
-    echo "############################################################"
-    echo "Importing image"
-    echo "############################################################"
+  echo "############################################################"
+  echo "Creating Project"
+  echo "############################################################"
+  
+  oc new-project oqss-cicd --display-name="CI/CD"
+  sleep 2
     
-    oc new-project oqss-cicd --display-name="CI/CD"
-    sleep 2
-    
-    oc new-app -f ./application/template.yaml -n oqss-cicd
+  oc new-app -f ./application/template.yaml -n oqss-cicd
 }
