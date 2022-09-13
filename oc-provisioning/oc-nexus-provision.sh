2c2df38670632e34589b613a7303569098ff7429
David S. Ferreira <74317650+davidsf026@users.noreply.github.com>
2022-09-13 14:59:51 -0300

    Update oc-nexus-provision.sh

--- oc-provisioning/oc-nexus-provision.sh
+++ oc-provisioning/oc-nexus-provision.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-echo "###############################################################################"
-echo "#  MAKE SURE YOU ARE LOGGED IN:                                               #"
-echo "#  $ oc login http://console.your.openshift.com                               #"
-echo "###############################################################################"
+command.deploy()
+{
+	echo "############################################################"
+	echo "Creating Project"
+	echo "############################################################"
 
-command.deploy() {
-  info "Creating namespaces $cicd_prj, $dev_prj, $stage_prj"  
-  oc new-project oqss-cicd --display-name="CI/CD"
-  sleep 2
-    
-  oc new-app -f ./application/template.yaml -n oqss-cicd
+	oc new-project oqss-cicd --display-name="CI/CD"
+	sleep 2
+
+	oc new-app -f ./application/template.yaml -n oqss-cicd
 }
+
+command.help()
+{
+	echo "To Do!"
+}
+
+if [ "$1" = "help" ]
+then
+	command.help
+elif [ "$1" = "deploy" ]
+then
+	command.deploy
+else
+	echo "Invalid choice..."
+fi
